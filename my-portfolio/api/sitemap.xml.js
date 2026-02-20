31531063305e0833015ceb0f6fedb3a6c33a9e4d
Rohit Rajak <rohitpictures2005@gmail.com>
2026-02-20 11:02:21 +0530

    minor

---
+++ my-portfolio/api/sitemap.xml.js
@@ -0,0 +1,26 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/xml");
+
+  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>https://rohitrajak.me/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://rohitrajak.me/projects</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://rohitrajak.me/about</loc>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+
+</urlset>`);
+}
